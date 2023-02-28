e1650e44ead7af8d2748fa2a8092160d4c0afcd6
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-28 15:10:48 +0300

    Updated Code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
+
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
   const show = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment'})
+    dispatch({ type: 'increment' })
   }
 
   const increaseHandler = () => {
